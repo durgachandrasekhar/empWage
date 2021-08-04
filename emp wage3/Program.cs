46dd88bfa1711f2b9c3a00cc0a4be1f7faabbe93
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-04 18:08:29 +0530

    feat:Added message

---
+++ emp wage3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace emp_wage3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PER_HOUR_WAGE = 1000;
+            int HOURS_WORKED = 8;
+            int dailyWage = 0;
+            Random rand = new Random();
+            int present = rand.Next(0, 2);
+            Console.WriteLine(present);
+            if (present == 1)
+            {
+                Console.WriteLine("Employee is presnt");
+                dailyWage = PER_HOUR_WAGE * HOURS_WORKED;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.WriteLine("Total daily wage earned" + dailyWage);
+        }
+    }
+}   
