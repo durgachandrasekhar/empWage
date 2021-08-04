15f083bc8157813cc6a88746f41c80787cec06cb
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-04 18:38:57 +0530

    feat:Add part time to empWage

--- emp wage3/Program.cs
+++ emp wage3/Program.cs
@@ -8,14 +8,20 @@ namespace emp_wage3
         {
             int PER_HOUR_WAGE = 1000;
             int HOURS_WORKED = 8;
+            int PART_TIME_HOURS = 4;
             int dailyWage = 0;
             Random rand = new Random();
-            int present = rand.Next(0, 2);
-            Console.WriteLine(present);
-            if (present == 1)
+            int workStatus = rand.Next(0, 3);    
+            ///Ctrl +R*R
+            if (workStatus == 1)
             {
                 Console.WriteLine("Employee is presnt");
                 dailyWage = PER_HOUR_WAGE * HOURS_WORKED;
+            }
+            else if (workStatus == 2)
+            {
+                Console.WriteLine("Employee has worked part time");
+                dailyWage = PER_HOUR_WAGE * PART_TIME_HOURS;
             }
             else
             {
